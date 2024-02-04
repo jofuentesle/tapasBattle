4d7926141dc239d8c7ca30cad3fe660c26d5db52
jofuentesl <jofuentesle@uoc.edu>
2024-02-04 23:10:45 +0100

    header show profile

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -29,7 +29,7 @@ export const routes: Routes = [
           
           {
           path: 'user',
-          component: PagesComponent,
+          component: UserComponent,
           children: [
     
           { path: '', 
