6768b1a50aaa7721e48605cc1cbdea52f8107ffb
jofuentesl <jofuentesle@uoc.edu>
2024-02-06 19:06:46 +0100

    link header

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -5,7 +5,6 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TapaComponent } from './tapa/tapa.component';
 import { UserComponent } from './user/user.component';
-import { ProfileComponent } from './user/profile/profile.component';
 import { VoteComponent } from './vote/vote.component';
 
 export const routes: Routes = [
@@ -34,10 +33,6 @@ export const routes: Routes = [
     
           { path: '', 
             component: UserComponent 
-          },  
-          {
-            path: 'profile',
-            component: ProfileComponent
           }
             ]
           }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from "@angular/router";
+
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
