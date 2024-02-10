9549efdd04ab44dbd7a5410ab5aa757f601d247a
jofuentesl <jofuentesle@uoc.edu>
2024-02-10 11:47:04 +0100

    update shared header 2

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from  '@angular/common/http';
+
 import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
@@ -11,8 +11,8 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     RegisterComponent,
-    LoginComponent,
-    HttpClientModule,
+    LoginComponent
+    
      
   ],
   imports: [
